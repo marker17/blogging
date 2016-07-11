2a17526a307bbf91028cefca9de60e0d4b924dd3
Mark Brillo <brillomark@yahoo.com>
2016-07-11 17:31:14 +0800

    fixed the slug bug

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,9 +28,9 @@ Route::get('/', 'PagesController@getIndex');
 
 Route::resource('posts', 'PostController');
 
-//Route::get('blog/{slug}', [
+Route::get('blog/{slug}', [
 
-//	'as' => 'blog.single', 'uses' => 'BlogController@getSingle'
-//]);
+	'as' => 'blog.single', 'uses' => 'BlogController@getSingle'
+])->where('slug', '[\w\d\-\_]+');
 
 
