4fbe70403e580ccef0e5be97c993150a1e777985
Mark Brillo <brillomark@yahoo.com>
2016-07-13 17:12:12 +0800

    fix error

--- config/database.php
+++ config/database.php
@@ -1,4 +1,10 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));                   
+$host = $url["host"];                   
+$username = $url["user"];                   
+$password = $url["pass"];                   
+$database = substr($url["path"], 1);                    
+
 
 return [
 
@@ -26,7 +32,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'mysql'
 
     /*
     |--------------------------------------------------------------------------
@@ -54,11 +60,15 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $host,
+            'username' => $username,
+            'database' => $database,
+            'password' => $password,
+            //'host' => env('DB_HOST', 'localhost'),
+            //'database' => env('DB_DATABASE', 'forge'),
+            //'username' => env('DB_USERNAME', 'forge'),
+            //'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
